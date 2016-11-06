ca8393dd6d4cc06e4f5de77a56abaa4dbcf521f3
igorwanbarros <igorwanbarros@gmail.com>
2016-11-06 17:09:11 -0300

    removendo Helpers (adicionando ao BaseLaravel) adicionando config messages melhorando app.blade removendo view form e index default (adicionados ao BaseLaravel)

---
+++ config/messages.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'create' => [
+        'success' => 'Registro adicionado',
+        'error' => 'Não consegui adicionar esta registro',
+    ],
+    'update' => [
+        'success' => 'Registro alterado',
+        'error' => 'Não consegui alterar este registro',
+    ],
+    'delete' => [
+        'success' => 'Registro excluído',
+        'error' => 'Não consegui excluir o registro',
+    ],
+];
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,23 @@
 @extends('templates.admin-lte.app')
 
+@section('styles')
+    @include('templates.admin-lte.styles')
+    <link rel="stylesheet" type="text/css" href="{{url('assets/base-laravel/base-laravel.css')}}">
+    <link rel="stylesheet" href="{{url('assets/base-laravel/toastr/toastr.min.css')}}">
+@stop
+
+@section('body-class')
+    hold-transition skin-red sidebar-mini sidebar-collapse
+@stop
+
 @section('sidebar')
     @include('templates.admin-lte.sidebar')
     @include('menu')
+@stop
+
+@section('scripts')
+    @include('templates.admin-lte.scripts')
+    <script src="{{url('assets/base-laravel/base-laravel.js')}}"></script>
+    <script src="{{url('assets/base-laravel/toastr/toastr.js')}}"></script>
+    {!! toastr()->message() !!}
 @stop
